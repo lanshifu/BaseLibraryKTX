02ab2204ff4b3999dd2e43a3d70eadb419c3a5f1
lanxiaobin <lanxiaobin@lizhi.fm>
2020-05-06 20:35:04 +0800

    test

--- app/src/main/java/com/lanshifu/baselibraryktx/fragmentstatus/TestFragment.kt
+++ app/src/main/java/com/lanshifu/baselibraryktx/fragmentstatus/TestFragment.kt
@@ -3,7 +3,10 @@ package com.lanshifu.baselibraryktx.fragmentstatus
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.lanshifu.lib.ext.logd
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 /**
  * @author lanxiaobin
@@ -15,6 +18,16 @@ class TestFragment() : Fragment() {
         super.onCreate(savedInstanceState)
 
         logd("TestFragment onCreate")
+
+        lifecycleScope.launch {
+
+            withContext(Dispatchers.Default) {
+                logd("TestFragment 进入协程代码块:${Thread.currentThread()}")
+            }
+
+            logd("TestFragment,协程代码块完成:${Thread.currentThread()}")
+
+        }
     }
 
     override fun onResume() {
